f69b774781cf572146c4d46ef56198f23f103819
elangelaar <ellenlangelaar@gmail.com>
2017-05-22 15:55:24 +0200

    Added gif support

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(function() {
-    start();
+    //startGihpy();
+    startGif();
 });
 
-function start(){
+function startGihpy(){
   $.getJSON( "http://api.giphy.com/v1/gifs/search?q=lick&api_key=dc6zaTOxFJmzC", function( data ) {
     $.each( data['data'], function( key, val ) {
       $(".giphy-container").append('<img src="' + val['images']['fixed_height']['url'] + '">');
     });
   });
+}
+
+function startGif(){
+	for(var i = 1; i < 12; i++) {
+		$(".gif-container").append('<img src="img/video-'+ i +'.gif">');
+	}
+	
 }
