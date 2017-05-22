8f6a8e84d1267a4784f3748518dfa0a7452d9964
elangelaar <ellenlangelaar@gmail.com>
2017-05-22 11:05:59 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VideoWall
 Video wall website
+
+
+TODO: Add video to gif converter
---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    start();
+});
+
+function start(){
+  $.getJSON( "http://api.giphy.com/v1/gifs/search?q=lick&api_key=dc6zaTOxFJmzC", function( data ) {
+    $.each( data['data'], function( key, val ) {
+      $(".giphy-container").append('<img src="' + val['images']['fixed_height']['url'] + '">');
+    });
+  });
+}
